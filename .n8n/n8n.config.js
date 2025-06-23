a4b7ad00dfe98cc20f2ffd0c28fa78aff78919ed
Natalia-31 <sedos.sedyakina21@gmail.com>
2025-06-23 10:08:58 +0300

    Create n8n.config.js

---
+++ .n8n/n8n.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  generic: {
+    baseUrl: 'https://n8n2-production-9917.up.railway.app',
+  },
+  webhook: {
+    tunnelUrl: 'https://n8n2-production-9917.up.railway.app',
+  },
+};
